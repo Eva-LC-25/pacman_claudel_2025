26d3d8fa290f853a9cdef364f10aae3412fdbbac
Eva-LC-25 <evalec@claudel.org>
2025-11-19 21:09:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ import math
 import random
 import pyxel
 pos_player = [76, 56]
+                    #background code starts
+pyxel.init(128, 128)
+pyxel.load("res.pyxres")
 
+def update():                
+    if pyxel.btnp(pyxel.KEY_Q):
+        pyxel.quit()
+def draw():
+    pyxel.cls(0)
+    pyxel.bltm(0, 0, 0, 0, 0, 128, 128)
+pyxel.run(update, draw)
+                    #background code ends
 fond = 8
 def update():
     #logique du jeu
@@ -24,13 +35,6 @@ def update():
 def draw():
     pyxel.cls(fond) #fond
     pyxel.rect(pos_player[0], pos_player[1], 4, 4, 11)
-    pyxel.rect(0, 110, 160, 10, 2) # name
-    pyxel.rect(0, 0, 160, 10, 2)
-    pyxel.rect(0, 0, 10, 50, 2)
-    pyxel.rect(0, 60, 10, 60, 2)
-    pyxel.rect(150, 0, 10, 50, 2)
-    pyxel.rect(150, 60, 10, 60, 2)
-    pyxel.rect(0, 50, 10 ,10 , 7)
 #     pyxel.rect(x_carre2, y_carre2, 4, 4, 3)
 pyxel.init(160, 120, title="mon premier jeu")
 pyxel.run(update, draw)
