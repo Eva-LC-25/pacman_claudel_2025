2fa62875abbe3e354a807084a1f776bbf1098aa8
sacvin-crypto <sacvin@claudel.org>
2025-12-19 23:31:14 -0500

    main update

--- main.py
+++ main.py
@@ -1,51 +1,97 @@
-# Pyxel Studio
 import pyxel
 import random
 
-pos_player = [76, 56]
-x= 56
-y= 70
-LCARRE = 4
+PAC_MONSIEUR = (0, 0, 16, 16)
 
-def update():
-    #logique du jeu
-    global pos_player,fond,x,y
-    dx = 0
-    dy = 0
-    #deplacement avec les fleches
-    if pyxel.btn(pyxel.KEY_RIGHT):
-        dx = 1
-    if pyxel.btn(pyxel.KEY_LEFT):
-        dx = -1
-    if pyxel.btn(pyxel.KEY_UP):
-        dy = -1
-    if pyxel.btn(pyxel.KEY_DOWN):
-        dy = 1
-    x+=dx
-    y+=dy
-    print (x,y)
-    
-    #collision
-    if pyxel.pget(x, y) == 8 or pyxel.pget(x + LCARRE - 1, y + LCARRE - 1) == 8:
-        x -= dx
-        y -= dy
-    
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
-    
-    #teleporting function
-    if x <= -3 and y in range (56,69):
-        x = 127
-    if x >= 128 and y in range (56,69):
-        x = -2
+FANTOME1 = (0, 16, 16, 16)
+FANTOME2 = (16, 16, 16, 16)
+FANTOME3 = (32, 16, 16, 16)
+FANTOME4 = (48, 16, 16, 16)
 
+PETIT_POINT = (0, 48, 16, 16)
+GROS_POINT = (16, 48, 16, 16)
 
-def draw():
-    pyxel.cls(0)
-    pyxel.bltm(0, 0, 0, 0, 0, 128, 128)
-    pyxel.rect(x, y, LCARRE, LCARRE, 9)
-#     pyxel.rect(x_carre2, y_carre2, 4, 4, 3)
+class App:
+    def __init__(self):
+        pyxel.init(160, 120, title="Pac-Claudel")
+
+       
+        pyxel.load("final-sprites.pyxres")
+        
+        self.x = 0
+        self.y = 0
+
+        
+        self.speed = 2
+        
+        self.fantomes = [
+            {"x": 0,  "y": 16, "dx": 1,  "dy": 0, "timer": 0, "sprite": FANTOME1},
+            {"x": 16, "y": 16, "dx": 0,  "dy": 1, "timer": 0, "sprite": FANTOME2},
+            {"x": 32, "y": 16, "dx": -1, "dy": 0, "timer": 0, "sprite": FANTOME3},
+            {"x": 48, "y": 16, "dx": 0,  "dy": -1, "timer": 0, "sprite": FANTOME4},
+        ]
+
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+        if pyxel.btn(pyxel.KEY_LEFT):
+            self.x -= self.speed
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            self.x += self.speed
+        if pyxel.btn(pyxel.KEY_UP):
+            self.y -= self.speed
+        if pyxel.btn(pyxel.KEY_DOWN):
+            self.y += self.speed
+            
+        fantomes_speed = 0.5
+        cercle_detection = 40
+
+        for i in self.fantomes:
+            dx = self.x - i["x"]
+            dy = self.y - i["y"]
+
     
-pyxel.init(128, 128, title="mon premier jeu")
-pyxel.load("res.pyxres")
-pyxel.run(update, draw)
+        if abs(dx) < cercle_detection and abs(dy) < cercle_detection:
+           
+            if dx > 0:
+                i["x"] += fantomes_speed
+            elif dx < 0:
+                i["x"] -= fantomes_speed
+
+            if dy > 0:
+                i["y"] += fantomes_speed
+            elif dy < 0:
+                i["y"] -= fantomes_speed
+        else:
+           
+            if i["timer"] <= 0:
+                i["dx"] = pyxel.rndi(-1, 1)
+                i["dy"] = pyxel.rndi(-1, 1)
+                i["timer"] = 30
+
+            i["x"] += i["dx"]
+            i["y"] += i["dy"]
+            i["timer"] -= 1
+        
+        
+        
+
+    def draw(self):
+        
+        pyxel.cls(0)
+
+        pyxel.blt(self.x, self.y, 0, *PAC_MONSIEUR, 0)
+     
+        for i in self.fantomes:
+            pyxel.blt(i["x"], i["y"], 0, *i["sprite"], 0)
+
+        
+        pyxel.blt(0, 48, 0, *PETIT_POINT, 0)
+        pyxel.blt(16, 48, 0, *GROS_POINT, 0)
+        
+        for i in self.fantomes:
+            pyxel.blt(i["x"], i["y"], 0, *i["sprite"], 0)
+
